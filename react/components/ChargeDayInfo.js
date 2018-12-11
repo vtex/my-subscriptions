2bdd0238f0c0f68ded6fd6ba98e0a25a2d8206cd
Felipe Sales <sales.felipeb@gmail.com>
2018-12-11 00:20:22 -0200

    Fix FrequencyInfo label

--- react/components/ChargeDayInfo.js
+++ react/components/ChargeDayInfo.js
@@ -3,29 +3,34 @@ import PropTypes from 'prop-types'
 import { injectIntl, intlShape } from 'react-intl'
 import { compose, withProps } from 'recompose'
 
-import { subscriptionShape } from '../proptypes'
+import LabeledInfo from './LabeledInfo'
 
-const ChargeDayInfo = ({ intl, subscription, shouldBeEmpty }) => {
-  let chargeInfo
-  if (subscription.plan.frequency.periodicity === 'WEEKLY') {
-    chargeInfo = intl.formatMessage({
-      id: `subscription.periodicity.${subscription.purchaseSettings.purchaseDay.toLowerCase()}`,
-    })
-  } else chargeInfo = subscription.purchaseSettings.purchaseDay
+const ChargeDayInfo = ({ intl, periodicity, purchaseDay, shouldBeEmpty }) => {
+  const chargeInfo =
+    periodicity === 'WEEKLY'
+      ? intl.formatMessage({
+          id: `subscription.periodicity.${purchaseDay.toLowerCase()}`,
+        })
+      : purchaseDay
 
-  return (
+  const label = (
     <Fragment>
-      <span className="b db f6 c-on-base">
-        {!shouldBeEmpty && intl.formatMessage({ id: 'subscription.data.chargeDay' })} &nbsp;
-      </span>
-      <span className="fw3 db f5-ns f6-s c-on-base">{!shouldBeEmpty && chargeInfo} &nbsp;</span>
+      {!shouldBeEmpty &&
+        intl.formatMessage({ id: 'subscription.data.chargeDay' })}
+      &nbsp;
     </Fragment>
+  )
+  return (
+    <LabeledInfo label={label}>
+      {!shouldBeEmpty && chargeInfo} &nbsp;
+    </LabeledInfo>
   )
 }
 
 ChargeDayInfo.propTypes = {
   intl: intlShape.isRequired,
-  subscription: subscriptionShape.isRequired,
+  periodicity: PropTypes.string.isRequired,
+  purchaseDay: PropTypes.string.isRequired,
   shouldBeEmpty: PropTypes.bool.isRequired,
 }
 
