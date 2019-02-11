d9efd051f0b3b0e3a98b9f32b7278631dfd99c54
Felipe Sales <sales.felipeb@gmail.com>
2019-02-11 16:43:17 -0200

    Fix loading component

---
+++ react/components/pages/List/DescriptionColumnSkeleton.tsx
@@ -0,0 +1,22 @@
+import React, { FunctionComponent } from 'react'
+
+import SkeletonLoader from '../../commons/SkeletonLoader'
+
+const DescriptionColumnSkeleton: FunctionComponent = () => {
+  return (
+    <div className="ml4">
+      <SkeletonLoader width="70" size="3" />
+      <div className="mt3">
+        <SkeletonLoader width="60" size="3" />
+      </div>
+      <div className="mt6-ns mt4-s">
+        <SkeletonLoader width="70" size="3" />
+        <div className="mt3">
+          <SkeletonLoader width="60" size="3" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionColumnSkeleton
--- react/components/pages/List/SubscriptionsGroups.tsx
+++ react/components/pages/List/SubscriptionsGroups.tsx
@@ -1,21 +1,22 @@
-import React, { FunctionComponent }from 'react'
+import React, { FunctionComponent } from 'react'
 import { compose } from 'recompose'
 
 import { SubscriptionStatus } from '../../../enums'
 import GET_SUBSCRIPTIONS from '../../../graphql/getGroupedSubscriptions.gql'
 import withQuery from '../../hocs/withQuery'
+import Loading from './Loading'
 
 interface Props {
   filter: SubscriptionStatus[]
 }
 
 const SubscriptionsGroups: FunctionComponent<Props> = ({ filter })  => {
-  console.log(filter)
-  return <div>hey</div>
+  console.log('filter', filter)
+  return <Loading />
 }
 
 const enhance = compose<any, Props>(
-  withQuery({ document: GET_SUBSCRIPTIONS, errorCallback: (e) => console.log(e), loadingState: () => <div>Loading</div>, errorState: () => <div>Error</div> })
+  withQuery({ document: GET_SUBSCRIPTIONS, errorCallback: (e) => console.log(e), loadingState: Loading, errorState: () => <div>Error</div> })
 )
 
 export default enhance(SubscriptionsGroups)
