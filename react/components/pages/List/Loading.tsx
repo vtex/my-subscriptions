49d2a2725f49c311efc51a33bab7385e5e251a55
Felipe Sales <sales.felipeb@gmail.com>
2019-02-15 17:17:19 -0200

    Improve tslint ignore line and using css explicitily

--- react/components/pages/List/Loading.tsx
+++ react/components/pages/List/Loading.tsx
@@ -4,19 +4,17 @@ import css from '../../../constants/css'
 import SkeletonLoader from '../../commons/SkeletonLoader'
 import DescriptionColumn from './SkeletonDescriptionColumn'
 
-const { subscriptionGroupItemWrapper, subscriptionGroupImageWrapper } = css
-
 const SubscriptionsGroupsLoading: FunctionComponent = () => {
   return (
     <div className="w-100">
       {[0, 1].map(index => (
-        <div className={subscriptionGroupItemWrapper}  key={index}>
-          <div className={subscriptionGroupImageWrapper}>
+        <div className={css.subscriptionGroupItemWrapper} key={index}>
+          <div className={css.subscriptionGroupImageWrapper}>
             <SkeletonLoader />
           </div>
           <div className="ml6-ns ml0-s mt0-ns mt3-s w-100 mb3">
             <div className="mv7 w-40-ns w-70-s">
-              <SkeletonLoader size="4"/>
+              <SkeletonLoader size="4" />
             </div>
             <div className="mt3 w-70-ns w-100-s flex flex-row-s flex-column flex-nowrap flex-wrap-s">
               <div className="w-50-ns w-100-s">
