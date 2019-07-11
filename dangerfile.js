d55508f5046698d6ab1e88015396104b8ded21c8
Christian Kaisermann <christian@kaisermann.me>
2019-07-11 16:39:29 -0300

    Beeeeh

--- dangerfile.js
+++ dangerfile.js
@@ -1,28 +1,3 @@
-const assert = require('@vtex/danger')
-// const config = require('@vtex/danger/config')
+const { assert } = require('@vtex/danger')
 
-// Level of checks
-const FAIL = 'fail'
-const WARN = 'warn'
-const INFO = 'message'
-
-const config = {
-  matchers: {
-    test_files: path => path.match(/test|spec/i),
-    code_files: path => path.match(/\.[tj]sx?$/i),
-  },
-  rules: {
-    file_changes: [INFO, { created: true, deleted: true, modified: true }],
-    changelog: FAIL,
-    description: [FAIL, { minLength: 20 }],
-    wip: FAIL,
-    assignee_reviewers: WARN,
-    pr_size: [WARN, { additionLimit: 800, deletionLimit: -1 }],
-    lock_file: WARN,
-    need_rebase: WARN,
-    tests_only: FAIL,
-    console_log: WARN,
-  },
-}
-
-assert(config)
+assert()
