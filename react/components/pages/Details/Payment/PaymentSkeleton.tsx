2c4ad9ad47902eacb6a2137121c47f3acf16ddaf
Felipe Sales <sales.felipeb@gmail.com>
2020-05-12 15:31:37 -0300

    Fix loaders

--- react/components/pages/Details/Payment/PaymentSkeleton.tsx
+++ react/components/pages/Details/Payment/PaymentSkeleton.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 
-import { CSS, BASIC_CARD_WRAPPER } from '../../../../constants'
 import SkeletonLoader from '../../../commons/SkeletonLoader'
 
 const PaymentSkeleton = () => (
-  <div className={`${BASIC_CARD_WRAPPER} ${CSS.cardHorizontalPadding}`}>
+  <>
     <div className="db-s di-ns b w-90">
       <SkeletonLoader width="20" size="3" />
     </div>
     <div className="w-100 dib-ns pt6">
       <SkeletonLoader width="10" size="3" />
     </div>
-  </div>
+  </>
 )
 
 export default PaymentSkeleton
