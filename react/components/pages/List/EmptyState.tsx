4c75ce392ee81824889a8762f5daa5ea4de216e5
Felipe Sales <sales.felipeb@gmail.com>
2020-05-21 16:25:50 -0300

    Fix list messages

--- react/components/pages/List/EmptyState.tsx
+++ react/components/pages/List/EmptyState.tsx
@@ -1,15 +1,18 @@
 import React, { FunctionComponent } from 'react'
-import { InjectedIntlProps, injectIntl } from 'react-intl'
+import { InjectedIntlProps, injectIntl, defineMessages } from 'react-intl'
 import { EmptyState } from 'vtex.styleguide'
+
+const messages = defineMessages({
+  title: { id: 'subscriptions.notFound.title', defaultMessage: '' },
+  body: { id: 'subscriptions.notFound.text', defaultMessage: '' },
+})
 
 const EmptyStateSubscriptionsGroupList: FunctionComponent<InjectedIntlProps> = ({
   intl,
 }) => {
   return (
-    <EmptyState
-      title={intl.formatMessage({ id: 'subscriptions.notFound.title' })}
-    >
-      {intl.formatMessage({ id: 'subscriptions.notFound.text' })}
+    <EmptyState title={intl.formatMessage(messages.title)}>
+      {intl.formatMessage(messages.body)}
     </EmptyState>
   )
 }
