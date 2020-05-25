2e8f81e8e32dadf092ed8d8b500d02bf7829cfd9
Felipe Sales <sales.felipeb@gmail.com>
2020-05-25 16:45:06 -0300

    Using totalizer translator on SubscriptionTotals...

--- react/components/Details/SubscriptionTotals.tsx
+++ react/components/Details/SubscriptionTotals.tsx
@@ -1,28 +1,13 @@
 import React, { FunctionComponent } from 'react'
-import { injectIntl, InjectedIntlProps, defineMessages } from 'react-intl'
+import { FormattedMessage } from 'react-intl'
 import { Total } from 'vtex.subscriptions-graphql'
+import { TranslateTotalizer } from 'vtex.totalizer-translator'
 
 import FormattedPrice from './FormattedPrice'
-
-const messages = defineMessages({
-  total: { id: 'store/subscription.summary.total', defaultMessage: '' },
-  // TODO: use Totatlizer translation
-  orderId: { id: 'store/subscription.summary.orderId', defaultMessage: '' },
-  value: { id: 'store/subscription.summary.value', defaultMessage: '' },
-  totalValue: {
-    id: 'store/subscription.summary.totalValue',
-    defaultMessage: '',
-  },
-  items: { id: 'store/subscription.summary.items', defaultMessage: '' },
-  shipping: { id: 'store/subscription.summary.shipping', defaultMessage: '' },
-  discounts: { id: 'store/subscription.summary.discounts', defaultMessage: '' },
-  quantity: { id: 'store/subscription.summary.quantity', defaultMessage: '' },
-})
 
 const SubscriptionTotals: FunctionComponent<Props> = ({
   totals,
   currencyCode,
-  intl,
 }) => {
   const fullPrice = totals.reduce((price, total) => price + total.value, 0)
 
@@ -32,9 +17,7 @@ const SubscriptionTotals: FunctionComponent<Props> = ({
         return (
           <div className="cf pt2" key={total.id}>
             <div className="dib f6 fw4 c-muted-1 w-40">
-              {intl.formatMessage({
-                id: `store/subscription.summary.${total.id.toLowerCase()}`,
-              })}
+              <TranslateTotalizer id={total.id} />
             </div>
             <div className="dib f6 fw4 c-muted-1 tr w-60">
               <FormattedPrice value={total.value} currency={currencyCode} />
@@ -42,10 +25,9 @@ const SubscriptionTotals: FunctionComponent<Props> = ({
           </div>
         )
       })}
-
       <div className="pt2">
         <div className="dib fl f6 fw5 c-muted-1 w-40">
-          {intl.formatMessage(messages.total)}
+          <FormattedMessage id="store/subscription.summary.totalValue" />
         </div>
         <div className="dib f6 fw5 c-muted-1 w-60 tr">
           <FormattedPrice value={fullPrice} currency={currencyCode} />
@@ -55,9 +37,9 @@ const SubscriptionTotals: FunctionComponent<Props> = ({
   )
 }
 
-interface Props extends InjectedIntlProps {
+interface Props {
   totals: Total[]
   currencyCode: string
 }
 
-export default injectIntl(SubscriptionTotals)
+export default SubscriptionTotals
---
+++ react/typings/vtex.totalizer-translator.d.ts
@@ -0,0 +1,3 @@
+declare module 'vtex.totalizer-translator' {
+  export const TranslateTotalizer
+}
