7c8487fc8e1a42275e488ba284adae3ee1fa9f9a
Felipe Sales <sales.felipeb@gmail.com>
2020-05-25 11:59:00 -0300

    Add specific type definition for each graphql file

---
+++ react/typings/graphql/mutations/updateStatus.gql.d.ts
@@ -0,0 +1,6 @@
+declare module '*/updateStatus.gql' {
+  import { DocumentNode } from 'graphql'
+
+  const value: DocumentNode
+  export default value
+}
---
+++ react/typings/graphql/queries/subscriptionsGroup.gql.d.ts
@@ -0,0 +1,6 @@
+declare module '*/subscriptionsGroup.gql' {
+  import { DocumentNode } from 'graphql'
+
+  const value: DocumentNode
+  export default value
+}
