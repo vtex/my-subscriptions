15f582697b4d51ccd0062ab27e2dd21e4cd12a45
Felipe Sales <sales.felipeb@gmail.com>
2020-08-28 15:48:53 -0300

    Add SkuThumbnail

--- react/components/AddItemModal/SearchItem.tsx
+++ react/components/AddItemModal/SearchItem.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, useState } from 'react'
 import { FormattedNumber, FormattedMessage } from 'react-intl'
 
-import Image from '../ProductImage'
+import Thumbnail from '../SkuThumbnail'
 import { AddItemArgs } from '.'
 import Button from './SubscribeButton'
 import { subscribed, subscribable } from './SubscribeButton/utils'
@@ -26,57 +26,45 @@ const SearchItem: FunctionComponent<Props> = ({
   const canAdd = subscribable({ targetPlan, availablePlans })
 
   return (
-    <article className="flex">
-      <Image
-        imageUrl={imageUrl}
-        productName={name}
-        width={64}
-        height={64}
-        isFixed
-      />
-      <div className="w-100 flex flex-column flex-row-l justify-between pl4">
-        <div className="w-40-l w-100 mb0-l mb2">
-          <div className="c-muted-1 fw5 ttu f7 mb2">{brand}</div>
-          <div className="mb4 fw5">{name}</div>
-          <div className="t-small c-muted-1">
-            {`${unitMultiplier} ${measurementUnit}`}
-          </div>
-        </div>
-        <div className="w-60-l w-100 flex flex-column flex-row-l items-center-l justify-between-l">
-          {canAdd && (
-            <>
-              <QuantitySelector
-                id="search-modal"
-                value={quantity}
-                onChange={setQuantity}
-                disabled={
-                  subscribed({ skuId: id, subscribedSkus }) ||
-                  !subscribable({ targetPlan, availablePlans })
-                }
-              />
-              <span className="mv4 mv0-l">
-                <FormattedNumber
-                  currency={currency}
-                  style="currency"
-                  value={price * quantity}
-                />
-              </span>
-            </>
-          )}
-          <Button
-            skuId={id}
-            availablePlans={availablePlans}
-            targetPlan={targetPlan}
-            subscribedSkus={subscribedSkus}
-            buttonType="plain"
-            isLoading={loading}
-            onClick={() => onAddItem({ skuId: id, quantity, setLoading })}
-          >
-            <FormattedMessage id="store/add-item-modal.subscribe" />
-          </Button>
-        </div>
-      </div>
-    </article>
+    <Thumbnail
+      imageUrl={imageUrl}
+      name={name}
+      brandName={brand}
+      measurementUnit={measurementUnit}
+      unitMultiplier={unitMultiplier}
+    >
+      {canAdd && (
+        <>
+          <QuantitySelector
+            id="search-modal"
+            value={quantity}
+            onChange={setQuantity}
+            disabled={
+              subscribed({ skuId: id, subscribedSkus }) ||
+              !subscribable({ targetPlan, availablePlans })
+            }
+          />
+          <span className="mv4 mv0-l">
+            <FormattedNumber
+              currency={currency}
+              style="currency"
+              value={price * quantity}
+            />
+          </span>
+        </>
+      )}
+      <Button
+        skuId={id}
+        availablePlans={availablePlans}
+        targetPlan={targetPlan}
+        subscribedSkus={subscribedSkus}
+        buttonType="plain"
+        isLoading={loading}
+        onClick={() => onAddItem({ skuId: id, quantity, setLoading })}
+      >
+        <FormattedMessage id="store/add-item-modal.subscribe" />
+      </Button>
+    </Thumbnail>
   )
 }
 
