650a2ddbf1671af2a638551e3233310af454309e
Felipe Sales <sales.felipeb@gmail.com>
2020-09-04 07:39:23 -0300

    Fix Shedule_updated message on history

--- react/components/DetailsPage/History/utils.ts
+++ react/components/DetailsPage/History/utils.ts
@@ -4,47 +4,39 @@ import { SubscriptionExecutionStatus } from 'vtex.subscriptions-graphql'
 defineMessages({
   triggered: {
     id: 'store/subscription.execution.status.TRIGGERED',
-    defaultMessage: '',
   },
   inProcess: {
     id: 'store/subscription.execution.status.IN_PROCESS',
-    defaultMessage: '',
   },
   failure: {
     id: 'store/subscription.execution.status.FAILURE',
-    defaultMessage: '',
   },
   orderError: {
     id: 'store/subscription.execution.status.ORDER_ERROR',
-    defaultMessage: '',
   },
   success: {
     id: 'store/subscription.execution.status.SUCCESS',
-    defaultMessage: '',
   },
   expired: {
     id: 'store/subscription.execution.status.EXPIRED',
-    defaultMessage: '',
   },
   paymentError: {
     id: 'store/subscription.execution.status.PAYMENT_ERROR',
-    defaultMessage: '',
   },
   skipped: {
     id: 'store/subscription.execution.status.SKIPED',
-    defaultMessage: '',
   },
   noOrder: {
     id: 'store/subscription.execution.status.SUCCESS_WITH_NO_ORDER',
-    defaultMessage: '',
   },
   partial: {
     id: 'store/subscription.execution.status.SUCCESS_WITH_PARTIAL_ORDER',
-    defaultMessage: '',
   },
   reTriggered: {
     id: 'store/subscription.execution.status.RE_TRIGGERED',
-    defaultMessage: '',
+  },
+  scheduleUpdate: {
+    id: 'store/subscription.execution.status.SCHEDULE_UPDATED',
   },
 })
 
