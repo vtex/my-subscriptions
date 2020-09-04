193a17f0486a4897ac78ac0524390bacbe5c4878
Felipe Sales <sales.felipeb@gmail.com>
2020-09-04 08:21:32 -0300

    Remove unnused queries and mutations

--- react/typings/graphql/mutations/retry.gql.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*/retry.gql' {
-  import { DocumentNode } from 'graphql'
-  import { MutationRetryArgs as Args } from 'vtex.subscriptions-graphql'
-
-  export { Args }
-
-  const value: DocumentNode
-  export default value
-}
--- react/typings/graphql/queries/orderFormId.gql.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*/orderFormId.gql' {
-  import { DocumentNode } from 'graphql'
-
-  const value: DocumentNode
-  export default value
-}
