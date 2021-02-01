ee93f74aa0a3a440b5f10a767613206cde60adb8
Rafaella <rafaella.ribeiro@vtex.com.br>
2021-02-01 19:06:44 -0300

    Change purchase day selector - weekly

--- vtex-my-subscriptions-3/react/components/Selector/Frequency.tsx
+++ vtex-my-subscriptions-3/react/components/Selector/Frequency.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, FocusEvent, ReactNode } from 'react'
 import { injectIntl, WrappedComponentProps, defineMessages } from 'react-intl'
-import { Dropdown, ButtonGroup, Button } from 'vtex.styleguide'
+import { Dropdown } from 'vtex.styleguide'
 import { Frequency } from 'vtex.subscriptions-graphql'
 
 import {
@@ -50,68 +50,42 @@ const FrequencySelector: FunctionComponent<Props> = ({
 
   return (
     <>
-      <div style={{ maxWidth: '220px' }}>
-        <Dropdown
-          name="frequency"
-          label={intl.formatMessage(messages.orderAgain)}
-          placeholder={intl.formatMessage(messages.select)}
-          options={getFrequencyOptions({
-            intl,
-            frequencies: availableFrequencies,
-          })}
-          value={hasFrequency ? selectedFrequency : null}
-          onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-            onChangeFrequency(e.target.value)
-          }
-          onBlur={onBlurFrequency}
-          errorMessage={errorMessageFrequency}
-        />
-      </div>
-      {availableFrequencies.length !== 0 &&
-        currentFrequency &&
-        String(currentFrequency?.periodicity) === 'WEEKLY' && (
-          <div className="pt3">
-            <p className="mb3 t-small">
-              {intl.formatMessage(messages.chargeEvery)}
-            </p>
-            <ButtonGroup
-              buttons={getIntervalOptions({
-                intl,
-                periodicity: 'WEEKLY',
-              }).map((day) => (
-                <Button
-                  key={day.value}
-                  size="small"
-                  isActiveOfGroup={day.value === selectedPurchaseDay}
-                  onClick={() => onChangePurchaseDay(day.value)}
-                >
-                  {day.label.charAt(0)}
-                </Button>
-              ))}
-            />
-          </div>
-        )}
-      {availableFrequencies.length !== 0 &&
-        currentFrequency &&
-        (String(currentFrequency?.periodicity) === 'MONTHLY' ||
-          String(currentFrequency?.periodicity) === 'YEARLY') && (
-          <div className="pt6" style={{ width: '100px' }}>
-            <Dropdown
-              name="purchaseDay"
-              label={intl.formatMessage(messages.chargeEvery)}
-              options={getIntervalOptions({
-                intl,
-                periodicity: 'MONTHLY',
-              })}
-              value={selectedPurchaseDay}
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-                onChangePurchaseDay(e.target.value)
-              }
-              onBlur={onBlurPurchaseDay}
-              errorMessage={errorMessagePurchaseDay}
-            />
-          </div>
-        )}
+      <Dropdown
+        name="frequency"
+        label={intl.formatMessage(messages.orderAgain)}
+        placeholder={intl.formatMessage(messages.select)}
+        options={getFrequencyOptions({
+          intl,
+          frequencies: availableFrequencies,
+        })}
+        value={hasFrequency ? selectedFrequency : null}
+        onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+          onChangeFrequency(e.target.value)
+        }
+        onBlur={onBlurFrequency}
+        errorMessage={errorMessageFrequency}
+      />
+      {availableFrequencies.length !== 0 && currentFrequency && (
+        <div className="pt6">
+          <Dropdown
+            name="purchaseDay"
+            label={intl.formatMessage(messages.chargeEvery)}
+            options={getIntervalOptions({
+              intl,
+              periodicity:
+                String(currentFrequency?.periodicity) === 'WEEKLY'
+                  ? 'WEEKLY'
+                  : 'MONTHLY',
+            })}
+            value={selectedPurchaseDay}
+            onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+              onChangePurchaseDay(e.target.value)
+            }
+            onBlur={onBlurPurchaseDay}
+            errorMessage={errorMessagePurchaseDay}
+          />
+        </div>
+      )}
     </>
   )
 }
